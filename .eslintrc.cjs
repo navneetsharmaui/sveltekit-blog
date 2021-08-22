779799e77441ee20620e50b2963979594386a3d5
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-08-22 14:04:08 +0530

    Added the initial project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+/* jshint esversion: 9 */
+const typescript = require('typescript');
+
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:jest-dom/recommended'],
+	plugins: ['svelte3', '@typescript-eslint', 'jest-dom'],
+	ignorePatterns: ['*.cjs'],
+	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	settings: {
+		'svelte3/typescript': () => typescript,
+		'svelte3/ignore-styles': () => true,
+	},
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2020,
+	},
+	env: {
+		browser: true,
+		node: true,
+		es2020: true,
+	},
+	rules: {
+		semi: ['error', 'always'],
+		'no-console': ['warn'],
+	},
+};
