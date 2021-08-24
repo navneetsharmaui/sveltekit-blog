3ca80616662b8f137ddd869ace20ce0a883083c0
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-08-24 21:58:55 +0530

    Removed the todo route and added the blog route

---
+++ src/routes/blog/index.svelte
@@ -0,0 +1,59 @@
+<script lang="ts" context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+
+</script>
+
+<script lang="ts">
+	// Start: Local Imports
+	// Components
+	import HeadTags from '$components/head-tags/HeadTags.svelte';
+
+	// Models
+	import type { IMetaTagProperties } from '$models/interfaces/imeta-tag-properties.interface';
+	// End: Local Imports
+
+	// Start: Local component properties
+	/**
+	 * @type {IMetaTagProperties}
+	 */
+	const metaData: Partial<IMetaTagProperties> = {
+		title: 'Blog | Sveltekit',
+		description: 'Blog page of Sveltekit starter blog project',
+		url: '/blog',
+		keywords: ['sveltekit', 'sveltekit starter blog', 'sveltekit starter'],
+		searchUrl: '/blog',
+	};
+	// End: Local component properties
+</script>
+
+<!-- Start: Header Tag -->
+<HeadTags metaData="{metaData}" />
+<!-- End: Header Tag -->
+
+<!-- Start: About page section -->
+<section class="text-gray-600 body-font">
+	<div class="container px-5 py-24 mx-auto">
+		<div class="flex flex-col text-center w-full mb-20">
+			<h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">About Page</h1>
+			<p class="lg:w-2/3 mx-auto leading-relaxed text-base"
+				>The template has all the route and sample pages setup for you which you can change and customize
+				further. All the pages are by default loaded from the different JS file and is fetched using prefetch
+				when use hover over the links, which reduces the load time when user eventually decides to click on the
+				link and load the route.</p
+			>
+		</div>
+	</div>
+</section>
+<!-- End: About page section -->
