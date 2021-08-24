7b0b25eedff0ffbbdacd27486ddc1d9b6cd43485
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-08-24 21:26:53 +0530

    Added the image optimization script

---
+++ scripts/js/optimize-image.ts
@@ -0,0 +1,17 @@
+import { writeFileSync } from 'fs';
+import { ImagePool } from '@squoosh/lib';
+
+const imagePool = new ImagePool();
+const [img] = process.argv.slice(2);
+
+const image = imagePool.ingestImage(img);
+const imageFunction = async () => {
+	await image.encode({
+		mozjpeg: {},
+	});
+	const { binary } = await image.encodedWith.mozjpeg;
+	await writeFileSync(img, binary);
+	await imagePool.close();
+};
+
+imageFunction().then().catch(console.error);
