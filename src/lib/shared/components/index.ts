f3d4e8836a526d526cf128dd684600c0c07101f7
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-01 13:32:48 +0530

    Removed the redundent component

--- src/lib/shared/components/index.ts
+++ src/lib/shared/components/index.ts
@@ -1,5 +1,4 @@
 export { ApplicationTitles } from './application-titles';
-export { Counter } from './counter';
 export { HeadTags } from './head-tags';
 export { Title } from './title';
 export { SEO } from './seo';
