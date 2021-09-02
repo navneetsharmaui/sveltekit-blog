ff7b43b7afce59332ccb54da2801cfc4d9758f82
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-02 12:30:32 +0530

    Added the seo head tag to the blogs

--- src/routes/blog/[slug]/index.svelte
+++ src/routes/blog/[slug]/index.svelte
@@ -14,11 +14,53 @@
 </script>
 
 <script lang="ts">
-	import BlogLayout from '$lib/layouts/blog-layout/BlogLayout.svelte';
-	import type { IBlogMD } from '$models/interfaces/iblog-md.interface';
+	// Start: Local Imports
+	// Components
+	import BlogLayout from '$layouts/blog-layout/BlogLayout.svelte';
+	import HeadTags from '$components/head-tags/HeadTags.svelte';
 
+	// Models
+	import type { IBlogMD } from '$models/interfaces/iblog-md.interface';
+	import type { IMetaTagProperties } from '$models/interfaces/imeta-tag-properties.interface';
+	// End: Local Imports
+
+	// Start: Exported Properties
 	export let blog!: IBlogMD;
+	// End: Exported Properties
+
+	// Start: Local component properties
+
+	/**
+	 * @type {IMetaTagProperties}
+	 */
+	let metaData: Partial<IMetaTagProperties> = {
+		title: `${blog.title} | Sveltekit`,
+		description:
+			'Sveltekit starter project created with sveltekit, typescript, tailwindcss, postcss, husky, and storybook. The project has the structure set up for the scaleable project. (sveltekit, typescript, tailwindcss, postcss, husky, Storybook).',
+		url: `/${blog.slug}`,
+		keywords: ['sveltekit', 'sveltekit starter', 'sveltekit starter users'],
+		searchUrl: `/${blog.slug}`,
+	};
+
+	// Start: Reactive properties
+	$: {
+		if (blog && blog.slug) {
+			metaData = {
+				title: `${blog.title} | Sveltekit`,
+				url: `/users/${blog.slug}`,
+				keywords: ['sveltekit', 'sveltekit-starter', 'sveltekit-starter-users', `sveltekit ${blog.title}`],
+				searchUrl: `/users/${blog.slug}`,
+			};
+		}
+	}
+	// End: Reactive properties
+
+	// End: Local component properties
 </script>
+
+<!-- Start: Header Tag -->
+<HeadTags metaData="{metaData}" />
+<!-- End: Header Tag -->
 
 <BlogLayout blog="{blog}">
 	{#if blog}
