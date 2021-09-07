b311c3a008fdaf70262c768fa197d32330b81a03
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-07 19:47:47 +0530

    Updated the theme mode style

--- src/lib/layouts/blog-layout/BlogLayout.svelte
+++ src/lib/layouts/blog-layout/BlogLayout.svelte
@@ -33,7 +33,13 @@
 	</h1>
 	<div class="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
 		<div class="flex items-center">
-			<img alt="{'Navneet Sharma'}" src="{'/images/author/navneet-sharma.jpg'}" class="rounded-full w-7 h-7" />
+			<img
+				alt="{'Navneet Sharma'}"
+				src="{'/images/author/navneet-sharma.jpg'}"
+				width="1.75rem"
+				height="1.75rem"
+				class="rounded-full w-7 h-7"
+			/>
 			<p class="text-sm text-gray-700 dark:text-gray-300 ml-2">
 				{blog.metadata.author}
 				{' / '}
