c1034b12842bfaf59c9be68dc495f0a438557380
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-07 13:27:14 +0530

    Added the functionality to add the anchor tags to the mardown heading

--- src/routes/blog/_blog-api.ts
+++ src/routes/blog/_blog-api.ts
@@ -1,3 +1,4 @@
+import { convertToSlug } from '$utils/convert-to-slug';
 import fs from 'fs';
 import frontMatter from 'front-matter';
 import marked from 'marked';
@@ -9,6 +10,15 @@ import readingTime from 'reading-time';
 loadLanguages(['shell', 'markdown', 'json']);
 
 const blogPath = './contents/blogs';
+const createAnchorAndFragment = (text) => {
+	const anchorRegExp = /{([^}]+)}/g;
+	const anchorOverwrite = anchorRegExp.exec(text);
+	const fragment = anchorOverwrite
+		? anchorOverwrite[0].substring(2, anchorOverwrite[0].length - 1)
+		: convertToSlug(text);
+
+	return { anchor: `#${fragment}`, fragment };
+};
 
 const posts = fs
 	.readdirSync(`${blogPath}`)
@@ -30,7 +40,16 @@ const posts = fs
 			const html = Prism.highlight(source, Prism.languages[lang], lang);
 			return `<pre class='language-${lang}'><code class='language-${lang}'>${html}</code></pre>`;
 		};
+		renderer.heading = (text, level, rawtext) => {
+			const headingText = text.includes('{') ? text.substring(0, text.indexOf('{') - 1) : text;
 
+			const { fragment } = createAnchorAndFragment(rawtext);
+			if (!fragment) {
+				return `<h${level} class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white">${headingText}</h${level}>`;
+			}
+
+			return `<h${level} class="font-bold text-2xl md:text-4xl tracking-tight mb-4 text-black dark:text-white" id="${fragment}"><a href="#${fragment}" class="anchor">${headingText}</a></h${level}>`;
+		};
 		const html = marked(postFrontMatter.body, { renderer });
 
 		const readingTimeDuration = readingTime(postFrontMatter.body).text;
