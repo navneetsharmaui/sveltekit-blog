bb653df7ff592da8a295d897a07a79f1094908ac
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-08 11:03:17 +0530

    Updated the lint config

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
 	'*.{js,ts,json,svelte}': ['npm run format', 'git add'],
-	'./src/**/*.{svelte,ts}': ['npm run lint:fix'],
+	'./src/**/*.{svelte,ts}': ['npm run format', 'npm run lint:fix'],
 	'./.{ts,cjs,js}': ['npm run lint:fix'],
 	'*.{jpg,jpeg,png,gif}': ['npm run optimize:image', 'git add'],
 };
