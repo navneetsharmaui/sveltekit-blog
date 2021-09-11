5dceec82a576701a64235f6ceacd7c078d2cccd9
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-11 21:17:31 +0530

    Removed the corrupted images

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,5 +4,5 @@ module.exports = {
 	'*.{js,ts,json,svelte}': ['npm run format', 'git add'],
 	'./src/**/*.{svelte,ts}': ['npm run format', 'npm run lint:fix'],
 	'./.{ts,cjs,js}': ['npm run lint:fix'],
-	'*.{jpg,jpeg,png,gif}': ['npm run optimize:image', 'git add'],
+	'*.{jpg,jpeg,gif}': ['npm run optimize:image', 'git add'],
 };
