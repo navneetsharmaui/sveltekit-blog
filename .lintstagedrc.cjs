4a5a171ce5c8ff1536d573ffe9722534be49626b
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-20 18:49:57 +0530

    Updated the lintstatgerc.cjs

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,7 +2,7 @@
 
 module.exports = {
 	'*.{js,ts,json,svelte}': ['npm run format', 'git add'],
-	'./src/**/*.{svelte,ts}': ['npm run format', 'npm run lint:fix'],
-	'./.{ts,cjs,js}': ['npm run lint:fix'],
-	'*.{jpg,jpeg,gif}': ['npm run optimize:image', 'git add'],
+	'./src/**/*.{svelte,ts}': ['npm run format', 'npm run lint:fix', 'git add'],
+	'./.{ts,cjs,js}': ['npm run lint:fix', 'git add'],
+	'*.{jpg,jpeg,gif,png}': ['npm run optimize:image', 'git add'],
 };
