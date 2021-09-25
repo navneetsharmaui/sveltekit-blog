192f9353aa492471d376198f8eded9b87a412089
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-25 21:08:09 +0530

    Added the moder image component

---
+++ src/lib/shared/ui/components/modern-image/ModernImage.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { IModernImage } from '$models/interfaces/imodern-image.interface';
+
+	export let modernImages!: IModernImage[];
+
+	$: modernJPEG = modernImages.find((x) => x.isJPEG);
+</script>
+
+<picture>
+	{#each modernImages as modernImage, index (modernImage.imageUrl)}
+		<source
+			srcset="{modernImage.imageUrl}"
+			type="{modernImage.imageType}"
+			width="{modernImage.imageWidth}"
+			height="{modernImage.imageHeight}"
+		/>
+	{/each}
+	<img
+		class="{modernJPEG.css}"
+		alt="{modernJPEG.imageAlt}"
+		loading="eager"
+		decoding="async"
+		width="{modernJPEG.imageWidth}"
+		height="{modernJPEG.imageHeight}"
+		src="{modernJPEG.imageUrl}"
+	/>
+</picture>
---
+++ src/lib/shared/ui/components/modern-image/index.ts
@@ -0,0 +1,1 @@
+export { default as ModernImage } from './ModernImage.svelte';
