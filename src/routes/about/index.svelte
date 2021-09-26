c6c400e690bd194e4026c15b77467f954f13389c
Navneet Sharma <navneetnnavneet1@gmail.com>
2021-09-26 21:45:54 +0530

    Updated the about page view

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -1,19 +1,3 @@
-<script lang="ts" context="module">
-	import { browser, dev } from '$app/env';
-
-	// we don't need any JS on this page, though we'll load
-	// it in dev so that we get hot module replacement...
-	export const hydrate = dev;
-
-	// ...but if the client-side router is already loaded
-	// (i.e. we came here from elsewhere in the app), use it
-	export const router = browser;
-
-	// since there's no dynamic data here, we can prerender
-	// it so that it gets served as a static asset in prod
-	export const prerender = true;
-</script>
-
 <script lang="ts">
 	// Start: Local Imports
 	// Components
@@ -44,15 +28,13 @@
 
 <!-- Start: About page section -->
 <div class="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
-	<h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 dark:text-white"> About Page </h1>
+	<h1 class="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white"> About Me </h1>
 	<div class="mb-8 prose leading-6 text-gray-600 dark:text-gray-400">
 		<p>
-			Hey, I'm Sveltekit Blogger. I'm a Software Developer, writer and creator of&nbsp;
+			Hey, I'm Sveltekit Blogger. I'm a Software Developer, writer and creator of
 			<ExternalLink href="https://github.com/navneetsharmaui/sveltkit-starter"
 				>Sveltekit Starter Project</ExternalLink
-			>
-			&nbsp;and&nbsp;
-			<ExternalLink href="https://github.com/navneetsharmaui/sveltkit-starter"
+			> and <ExternalLink href="https://github.com/navneetsharmaui/sveltkit-starter"
 				>Sveltekit Blog Project</ExternalLink
 			>
 		</p>
